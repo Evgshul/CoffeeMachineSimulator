a30bf7093804cfe6b99d64b93b6eb4754602099f
Evgeny <evgshul@gmail.com>
2019-02-20 16:05:56 +0200

    add more code

--- src/com/accenture/task/coffemashine/DrinkOrder.java
+++ src/com/accenture/task/coffemashine/DrinkOrder.java
@@ -1,20 +1,68 @@
 package com.accenture.task.coffemashine;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DrinkOrder implements CoffeMashine {
+
+    public static final String COFFEE = "coffee";
+    public static final String HOT_WATER = "hot water";
+    public static final String MILK = "milk";
+
     private Drink drinkType;
     private String name;
     private double money;
     private int sugar;
 
+    List<DrinkType> drinkTypes;
+
+    public DrinkOrder() {
+        List<String> blackCoffeeRecipe = new ArrayList<>();
+        blackCoffeeRecipe.add(COFFEE);
+        blackCoffeeRecipe.add(HOT_WATER);
 
 
-    @Override
-    public Drink make(DrinkOrder drinkOrder) {
-        return null;
+        List<String> cappucino = new ArrayList<>();
+        cappucino.add(COFFEE);
+        cappucino.add(HOT_WATER);
+        cappucino.add(MILK);
+
+
+        List<String> latte = new ArrayList<>();
+        latte.add(COFFEE);
+        latte.add(HOT_WATER);
+        latte.add(MILK);
+        List<DrinkType> drinkType = new ArrayList<>();
+        drinkType.add(new DrinkType("Black coffee", 0.50, blackCoffeeRecipe));
+        drinkType.add(new DrinkType("Cappuccino", 1.00, cappucino));
+        drinkType.add(new DrinkType("Latte", 1.00, latte));
+    }
+
+
+    public DrinkOrder(Drink drinkType, String name, double money, int sugar) {
+        this.drinkType = drinkType;
+        this.name = name;
+        this.money = money;
+        this.sugar = sugar;
+
+
     }
 
     @Override
-    public boolean validateOrder(String validate) {
+    public Drink make(DrinkOrder drinkOrder) {
+        System.out.println("Your " + drinkOrder.getName());
+        return drinkOrder.getDrinkType();
+    }
+
+    @Override
+    public boolean validateOrder(DrinkOrder order) {
+
+        for (DrinkType type : drinkTypes) {
+            if (type.equals(order.getName())) {
+                return true;
+            }
+        }
+
         return false;
     }
 
@@ -26,5 +74,37 @@ public class DrinkOrder implements CoffeMashine {
     @Override
     public boolean isBusy() throws InterruptedException {
         return false;
+    }
+
+    public Drink getDrinkType() {
+        return drinkType;
+    }
+
+    public void setDrinkType(Drink drinkType) {
+        this.drinkType = drinkType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public int getSugar() {
+        return sugar;
+    }
+
+    public void setSugar(int sugar) {
+        this.sugar = sugar;
     }
 }
